d6834746301fbc238841d0964b6c6257b5713429
cc <343126687@qq.com>
2025-07-25 17:00:21 +0800

    更新

--- links.md
+++ links.md
@@ -1,7 +1,7 @@
 ---
 title: 友链
 layout: links 
-date: 2025-10-01 10:16:09
+date: 2024-10-01 10:16:09
 banner:
   bannerText: A link to a friend's website
   bannerTitle: 莫愁前路无知己，<br>天下谁人不识君。
--- links.ts
+++ links.ts
@@ -3,13 +3,13 @@ export default [
 		name: "Cuirx",
 		url: "https://cuirx.de/",
 		image: "https://tc.cuirx.de/i/2025/07/24/ujrcee.webp",
-		desc: "醒，亦在人间；梦，亦在人间"
+		desc: "行百里者半九十"
 	},
     {
 		name: "vitepress",
 		url: "https://vitepress.dev/zh/",
 		image: "https://vitepress.dev/vitepress-logo-mini.svg",
-		desc: "醒，亦在人间；梦，亦在人间"
+		desc: "VitePress是一个静态站点生成器 (SSG)，专为构建快速、以内容为中心的站点而设计。"
 	},
     {
 		name: "白云苍狗",
