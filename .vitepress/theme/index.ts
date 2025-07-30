1d3088dcadadb7ea3906b48acbd9d2e7e2cf40f8
cc <343126687@qq.com>
2025-07-30 20:19:22 +0800

    美化代码块

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,8 @@
 import ThemeAsync from 'vitepress-theme-async';
 import Layout from './components/Layout.vue';
-import 'virtual:group-icons.css';
+import 'virtual:group-icons.css'
 import './custom.css'
+
 
 export default {
     ...ThemeAsync,
